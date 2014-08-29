d1321fb57aaef760bc546b28551f2e25300064cc
moof mayeda <moofmayeda@yahoo.com>
2014-08-29 16:07:32 -0700

    allow organizers to log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  def authorize
+    if current_organizer.nil?
+      flash[:alert] = "Not authorized"
+      redirect_to root_path
+    end
+  end
+
+  def current_organizer
+    @current_organizer ||= Organizer.find(session[:organizer_id]) if session[:organizer_id]
+  end
+  helper_method :current_organizer
 end
