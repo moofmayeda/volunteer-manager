bdd457877113556c98632c4f9878d5cf5c5717e0
moof mayeda <moofmayeda@yahoo.com>
2014-08-29 16:37:56 -0700

    test sign-in process

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -1,2 +1,21 @@
 require 'rails_helper'
 
+describe "the organizer signin process" do
+  it "signs an organizer in who uses the right password" do
+    visit '/sessions/new'
+    organizer = Organizer.create(:name => 'michael', :password => 'password')
+    fill_in 'Name', :with => 'michael'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign In'
+    expect(page).to have_content 'Add Event'
+  end
+
+  it "gives a user an error who uses the wrong password" do
+    visit '/sessions/new'
+    organizer = Organizer.create(:name => 'michael', :password => 'password')
+    fill_in 'Name', :with => 'michael'
+    fill_in 'Password', :with => 'wrong'
+    click_button 'Sign In'
+    expect(page).to have_content 'invalid'
+  end
+end
