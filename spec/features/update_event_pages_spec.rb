bc438f4aedbdad54a6deb587e81cee891f1ff468
moof mayeda <moofmayeda@yahoo.com>
2014-08-29 16:58:33 -0700

    test updating an event

---
+++ spec/features/update_event_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "updating an event" do
+  it "updates an event" do
+    visit '/sessions/new'
+    organizer = Organizer.create(:name => 'michael', :password => 'password')
+    fill_in 'Name', :with => 'michael'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign In'
+    visit '/events/new'
+    fill_in 'Name', :with => 'phone bank'
+    fill_in 'Date', :with => '2014-09-17'
+    fill_in 'Location', :with => 'union HQ'
+    click_button 'Create Event'
+    fill_in 'Name', :with => 'phone bank pizza party'
+    click_button 'Update Event'
+    expect(page).to have_content 'phone bank pizza party'
+  end
+end
